969cd5beae081058e6a51a7a1f280ad6fdc874c2
Amy Herring <amy.herring@duke.edu>
2018-05-25 09:14:25 -0400

    Amy H's first stab

---
+++ policies.Rmd.R
@@ -0,0 +1,151 @@
+---
+  title: "Policies "
+output: 
+  html_document:
+  toc_float: true
+---
+  
+  &nbsp
+&nbsp
+&nbsp
+&nbsp
+&nbsp
+&nbsp
+&nbsp
+&nbsp
+
+
+###Diversity & Inclusiveness:
+
+The Duke University Community Commitment states: ``Because diversity is essential to fulfilling the university’s mission, Duke is committed to building an inclusive and diverse university community.  Every student, faculty, and staff member —whatever their race, gender, age, ethnicity, cultural heritage or nationality; religious or political beliefs; sexual orientation or gender identity; or socioeconomic, veteran or ability status—has the right to inclusion, respect, agency and voice in the Duke community.  Further, all members of the University community have a responsibility to uphold these values and actively foster full participation in university life.''
+
+It is my goal for our learning environment to facilitate learning and intellectual development for everyone. To help accomplish this:
+  
+  - If you have a name and/or set of pronouns that differ from those that appear in your official Duke records, please let me know!
+  - We are constantly learning more about diverse perspectives and identities. If something was said in class (by anyone) that made you feel uncomfortable, please talk to me about it. 
+
+### Academic integrity:
+
+Duke University is a community dedicated to scholarship, leadership, and service and to the principles of honesty, fairness, respect, and accountability. Citizens of this community commit to reflect upon and uphold these principles in all academic and nonacademic endeavors, and to protect and promote a culture of integrity.
+
+Remember the Duke Community Standard that you have agreed to abide by:
+  
+  > To uphold the Duke Community Standard:
+  
+  > - I will not lie, cheat, or steal in my academic endeavors;
+> - I will conduct myself honorably in all my endeavors; and
+> - I will act if the Standard is compromised.
+
+Cheating on exams or plagiarism on homework assignments, lying about an illness or absence and other forms of academic dishonesty are a breach of trust with classmates and faculty, violate the <a href="http://www.studentaffairs.duke.edu/conduct/resources/dcs">Duke Community Standard</a>, and will not be tolerated. Such incidences will result in a 0 grade for all parties involved.  Additionally, there may be penalties to your final class grade along with being reported to the <a href="http://www.studentaffairs.duke.edu/conduct/about/ucbhearings">Undergraduate Conduct Board</a>.
+
+Please review the Academic Dishonesty policies <a href="https://studentaffairs.duke.edu/conduct">here</a>. 
+
+- Much of the work assigned in class is collaborative. The individual case study must be completed independently. In addition, regular assessments must be completed individually before they are tackled by the group. 
+
+**Referencing code:** The web contains an enormous volume of code that you may find useful. You are welcome to make use of any online resources (e.g. StackOverflow), but you must explicitly cite your sources. Any recycled code that is discovered and is not explicitly cited will be treated as plagiarism. On the individual case study you may not directly share code with another student in this class, and on team assignments you may not share code with another team in this class (you are welcome to discuss the problems together and ask for advice, but you may not send or make use of code from another team). 
+
+### Course components:
+
+#### Class sessions:
+
+If you miss class or would like to review the material covered in class, you can view the recordings [here](https://trinity.capture.duke.edu/Panopto/Pages/Sessions/List.aspx?folderID=9161c49e-b3d5-4d40-a282-ebe8a938055d). AMY UPDATE Note that you will need to log in with your Net ID.
+
+#### Teams:
+
+On the first day of class, we will develop teams of 5-7 students - these teams will stay consistent 
+throughout the semester (barring extraordinary circumstances). You will work in these teams during 
+class and on the five group case studies. The process of forming teams will be completely transparent 
+and based on self-administered personal skills assessments.
+
+#### Final Project:
+
+You will be responsible for the completion of an open ended final project for this course, 
+the goal of which is to tackle an "interesting" problem using the tools and techniques covered 
+in this class. Additional details on the project will be provided as the course progresses.
+
+Each team's work will also be shared with and evaluated by at least one other team at an 
+earlier stage in order to provide feedback in the form of code review. 
+
+#### Application exercises:
+
+Short exercises will frequently be administered at the begininning of class to assess progress and identify areas for further development. Students will first complete these individually and hand them in; subsequently, the group will discuss the exercise and turn in a group solution. 
+
+Students who miss individual and group submissions (due to late arrival or absence) will receive 0 points for those submissions. However, only the top 80% of submissions (separately for individual and group submissions) will count towards a student's point total.
+
+#### Homework:
+
+Beyond the in class activities, you will be assigned larger data analysis tasks throughout the semester. These assignments will be completed individually. 
+
+Homework with the lowest score for each student will be dropped.
+
+#### Case Studies:
+
+The objective of the  is to give you hands on experience with data analysis using modern statistical software. 
+
+All team members are expected to contribute equally to the completion of each assignment and you will be asked to evaluate your team members after a few assignments are due.
+
+Students are expected to make use of their team's git repository on the course's github page as their central collaborative platform. Commits to this repository will be used as a metric of each team member's relative contribution for each lab.
+
+Lab with the lowest score for each student will be dropped.
+
+#### Exams:
+
+There will be a two take home midterms that you are expected to complete individually. Each exam will ask you to complete a number of small programming and or analysis tasks related to the material presented in the class. The exams will be written to take between 2-5 hours. The exact structure and content of the exams will be discussed in more detail before they are assigned.
+
+
+
+#### Interactive tutorials:
+
+These are self-paced interactive tutorials that will be assigned intermittently throughout the semester. They will be graded on a check/no check basis (though you'll receive feedback on the way as you complete them) and they will count towards extra credit.
+
+### Grading:
+
+Your final grade will be comprised of the following:
+  
+{.table .table-condensed .table-striped .text-left}
+<span></span>        | <span></span>
+  ----------------------|----------------
+  Participation & application exercises       | 10%
+Peer evaluation       | 5%
+Homework              | 20%
+Labs                  | 15%
+Midterm 1             | 17.5%
+Midterm 2             | 17.5%
+Final project         | 15%
+
+Class attendance in lecture and lab is a firm expectation; frequent absences or tardiness will be considered a legitimate cause for grade reduction.
+
+Cumulative numerical averages of 90 - 100 are guaranteed at least an A-, 80 - 89 at least a B-, and 70 - 79 at least a C-, however the exact ranges for letter grades will be determined after the final exam. The more evidence there is that the class has mastered the material, the more generous the curve will be.
+
+### Excused Absences:
+
+Students who miss a class due to a scheduled varsity trip, religious holiday, or short-term illness should fill out an online <a href="http://trinity.duke.edu/academic-requirements?p=notification-of-varsity-athletic-participation-novap">NOVAP</a>, <a href="http://trinity.duke.edu/academic-requirements?p=religious-observance">RHoliday</a> or <a href="http://trinity.duke.edu/academic-requirements?p=policy-short-term-illness-notification">short-term illness</a> form respectively. Note that these excused absences do not excuse you from assigned homework, it is your responsibility to make alternative arrangements to turn in any assignments in a timely fashion.
+
+If you are faced with a personal or family emergency or a long-range or 
+chronic health condition that interferes with your ability to attend or 
+complete classes, you should contact your academic dean’s office. See more 
+information on policies surrounding these conditions 
+at https://trinity.duke.edu/undergraduate/academic-policies/personal-emergencies. 
+Your academic dean can also provide more information.
+
+### Late / missed work
+
+- Late work policy for homework assignments:
+  - late, but within 24 hours of due date/time: -20%
+- any later: no credit
+
+- Late work will not be accepted for take home midterms and the final project.
+
+- Exam dates cannot be changed and no make-up exams will be given. If a midterm exam must be missed, absence must be officially excused in advance of the due date, in which case the missing exam score will be imputed using the final exam score. This policy only applies to the midterms.
+
+- You must complete the final project and be in class to present it in order to pass this course.
+
+### Regrade requests
+
+Regrade requests must be made within three days of when the assignment is returned, and must be submitted via [this form](https://goo.gl/forms/tZPc5kB25O9jaMim2). These will be honored if points were tallied incorrectly, or if you feel your answer is correct but it was marked wrong. No regrade will be made to alter the number of points deducted for a mistake. There will be no grade changes after the final project presentations.
+
+### Professionalism
+
+- Please refrain from texting or using your computer for anything other than coursework during class.
+
+
